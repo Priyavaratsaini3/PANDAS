ea53b76914a2ec0d84274e4d7010a1f5c7506885
priyavaratsaini3 <priyavartsaini3@gmail.com>
2025-08-23 07:32:24 +0530

    load the list in the DataFrame

--- new.py
+++ new.py
@@ -1,7 +1,11 @@
 import pandas as pd
-df = pd.read_csv("kdrama_DATASET.csv", encoding="utf-8")
-# print(df)
-df1 = pd.read_excel("Historicalinvesttemp.xlsx")
-# print(df1)
-df2 = pd.read_json("titanic.json", lines=True)
-print(df2)
+# df = pd.read_csv("kdrama_DATASET.csv", encoding="utf-8")
+# # print(df)
+# df1 = pd.read_excel("Historicalinvesttemp.xlsx")
+# # print(df1)
+# df2 = pd.read_json("titanic.json", lines=True)
+# print(df2)
+list = [12,3,4,4,5,6]
+new = pd.DataFrame(list)
+print(new.head(5))
+
